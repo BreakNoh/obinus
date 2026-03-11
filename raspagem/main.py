9ceec3ca0dc68860a37abc7226ea23cefaa2df87
Breno <brematosmene@gmail.com>
2026-03-11 10:32:01 -0300

    inicia implementação da db

--- raspagem/main.py
+++ raspagem/main.py
@@ -1,11 +1,15 @@
 from common import Horario, Linha, salvar_csv
 import grande_floripa
+import sqlite3
+
+from tabela import iniciar_db
 
 todas_linhas: list[Linha] = []
 todos_horarios: list[Horario] = []
 
 for raspador in grande_floripa.todos:
     print(f"! raspando {raspador.empresa()}")
+
     linhas: list[Linha] = raspador.raspar_linhas()
 
     print(f"! {raspador.empresa()}: {len(linhas)} linhas raspadas")
@@ -22,10 +26,12 @@ for raspador in grande_floripa.todos:
     todas_linhas.extend(linhas)
     todos_horarios.extend(horarios)
 
-print("! salvando dados...")
+print("! salvando dados em csv...")
 
 salvar_csv(todas_linhas, "out/linhas.csv")
 salvar_csv(todos_horarios, "out/horarios.csv")
+
+print("! salvando dados na db")
 
 print("! dados salvos")
 
---
+++ raspagem/tabelas/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO empresas(regiao, nome, url)
+VALUES
+	('GRANDE_FLORIANOPOLIS', 'BIGUACU', 'https://www.tcbiguacu.com.br/'),
+	('GRANDE_FLORIANOPOLIS', 'FENIX', 'https://www.consorciofenix.com.br/'),
+	('GRANDE_FLORIANOPOLIS', 'ESTRELA', 'https://insulartc.com.br/est/wp/'),
+	('GRANDE_FLORIANOPOLIS', 'JOTUR', 'https://www.jotur.com.br/'),
+	('GRANDE_FLORIANOPOLIS', 'IMPERATRIZ', 'https://www.tcimperatriz.com.br/'),
+	('GRANDE_FLORIANOPOLIS', 'SANTA_TEREZINHA', 'https://santaterezinha.com/');
