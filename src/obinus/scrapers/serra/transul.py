3aa5fdb467d0c9dc8f8b9fd1d49b0cc28217c992
Breno <brematosmene@gmail.com>
2026-03-25 13:44:13 -0300

    adiciona scraper para transul

--- src/obinus/scrapers/serra/transul.py
+++ src/obinus/scrapers/serra/transul.py
@@ -1,12 +1,14 @@
+import re
 from obinus.core import Raspador, Linha, Horario
 from obinus.utils import get_soup, extrair_texto
 
 URL_LINHAS = "https://transullages.com.br/linhas"
-SELETOR_LINHAS = "tbody > tr"
+
+SELETOR_LINHAS = "table > tr"
 SELETOR_NOME = "td:first-child > a"
 SELETOR_CODIGO = "td:nth-child(2)"
 
-SELETOR_DIA = '[role="tablist"]'
+# SELETOR_DIA = '[role="tablist"]'
 SELETOR_DIA_UTIL = '[id="home-tab-pane"] .row > .col'
 SELETOR_DIA_SAB = '[id="profile-tab-pane"] .row > .col'
 SELETOR_DIA_DOM = '[id="contact-tab-pane"] .row > .col'
@@ -20,8 +22,10 @@ class Transul(Raspador):
     def raspar_linhas(self) -> list[Linha]:
         soup, _ = get_soup(URL_LINHAS)
         linhas = []
+        print(soup.prettify())
 
         for lin in soup.select(SELETOR_LINHAS):
+            print(lin.prettify())
             nome = extrair_texto(lin.select_one(SELETOR_NOME))
             codigo = extrair_texto(lin.select_one(SELETOR_CODIGO))
             url: str | None = None
@@ -41,4 +45,38 @@ class Transul(Raspador):
         return linhas
 
     def raspar_horarios_linha(self, linha: Linha) -> list[Horario]:
-        return super().raspar_horarios_linha(linha)
+        if linha.url == "":
+            return []
+        soup, _ = get_soup(linha.url)
+
+        dia_seletor: list[tuple[str, str]] = [
+            ("UTIL", SELETOR_DIA_UTIL),
+            ("SABADO", SELETOR_DIA_SAB),
+            ("DOMINGO_FERIADO", SELETOR_DIA_DOM),
+        ]
+        horarios = []
+
+        for dia, seletor in dia_seletor:
+            for col in soup.select(seletor):
+                sentido = extrair_texto(col.select_one(SELETOR_SENTIDO))
+                horas = extrair_texto(col.select_one(SELETOR_HORARIOS))
+
+                if not sentido or not horas:
+                    continue
+
+                padrao = r"[0-9]{2}:[0-9]{2}"
+
+                for hora in re.findall(padrao, horas):
+                    if hora:
+                        horarios.append(
+                            Horario(
+                                empresa=self.NOME_EMPRESA,
+                                linha=linha.codigo,
+                                sentido=sentido,
+                                hora=hora,
+                                dia=dia,
+                            )
+                        )
+
+        self.esperar()
+        return horarios
