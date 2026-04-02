691a6526b63070bed05afb45222f5af35287e5c7
BreaKnoh <brematosmene@gmail.com>
2026-04-02 16:38:20 -0300

    cria arquivos teste para estrela

---
+++ tests/tcestrela/test_tcestrela.py
@@ -0,0 +1,15 @@
+from utils import *
+from obinus.scrapers.grande_floripa.tcestrela import TCEstrela
+from obinus.core.tipos import *
+from pathlib import Path
+
+amos_linha, amos_horas = carregar_amostras(Path(__file__).parent)
+
+
+def test_extrair_linhas(): ...
+
+
+def test_extrair_legeda(): ...
+
+
+def test_extrair_horarios(): ...
