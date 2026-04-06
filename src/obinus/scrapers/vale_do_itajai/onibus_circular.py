72cd38305495dc8eddd8c77289cba8f156039980
BreaKnoh <brematosmene@gmail.com>
2026-04-06 20:22:58 -0300

    atualiza onibus circular

--- src/obinus/scrapers/vale_do_itajai/onibus_circular.py
+++ src/obinus/scrapers/vale_do_itajai/onibus_circular.py
@@ -1,7 +1,9 @@
-from obinus.scrapers.mobilibus import Mobilibus
+from obinus.core.tipos import VALE_DO_ITAJAI
+from obinus.scrapers.mobilibus import InterfaceMobilibus
 
 
-class OnibusCircular(Mobilibus):
-    NOME_EMPRESA = "ONIBUS_CIRCULAR"
+class OnibusCircular(InterfaceMobilibus):
+    NOME_EMPRESA = "Ônibus Circular"
     ID_PROJETO = "821"
     VERSAO_HORARIOS = "2"
+    REGIOES = VALE_DO_ITAJAI
