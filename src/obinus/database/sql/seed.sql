63cec7b872dbccc83664620ebfd6f7ea5d2cf784
BreaKnoh <brematosmene@gmail.com>
2026-09-08 11:18:02 -0300

    chore: remove arquivos não usados

--- src/obinus/database/sql/seed.sql
+++
@@ -1,9 +0,0 @@
-INSERT OR IGNORE INTO empresas(regiao, nome, url)
-VALUES
-	("TESTE", "SPEEDWAGON_FOUNDATION", "teste"),
-	('GRANDE_FLORIANOPOLIS', 'BIGUACU', 'https://www.tcbiguacu.com.br/'),
-	('GRANDE_FLORIANOPOLIS', 'FENIX', 'https://www.consorciofenix.com.br/'),
-	('GRANDE_FLORIANOPOLIS', 'ESTRELA', 'https://insulartc.com.br/est/wp/'),
-	('GRANDE_FLORIANOPOLIS', 'JOTUR', 'https://www.jotur.com.br/'),
-	('GRANDE_FLORIANOPOLIS', 'IMPERATRIZ', 'https://www.tcimperatriz.com.br/'),
-	('GRANDE_FLORIANOPOLIS', 'SANTA_TEREZINHA', 'https://santaterezinha.com/');
